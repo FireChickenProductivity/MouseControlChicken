e0cb19354c53f628c3fe8c69c3e1df92a48596f4
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-12 21:46:43 -0600

    Add grid class

---
+++ Grid.py
@@ -0,0 +1,5 @@
+from .fire_chicken.mouse_position import MousePosition
+
+class Grid:
+    def __init__(self, rectangle): pass
+    def compute_absolute_position(self, grid_coordinates: str) -> MousePosition: pass
