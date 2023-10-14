1ae56c878c44ee92324dbea14d507edd308d70c6
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-13 21:54:59 -0600

    Start on handling divisors

--- RectangleUtilities.py
+++ RectangleUtilities.py
@@ -7,6 +7,20 @@ def compute_center_position(rectangle: Rectangle) -> MousePosition:
     center = MousePosition(int(horizontal), int(vertical))
     return center
 
+def compute_sub_rectangle(rectangle: Rectangle, number_of_horizontal_divisors: int, number_of_vertical_divisors: int, horizontal: int, vertical: int):
+    pass
+
+def compute_line_divisors(start_amount: int, ending_amount: int, number_of_divisors: int):
+    if start_amount == ending_amount: return [start_amount]
+    division_size = int((ending_amount - start_amount)/(number_of_divisors + 1))
+    current_position = start_amount
+    result = []
+    for divisor in range(number_of_divisors):
+        if current_position > ending_amount: break
+        total += division_size
+        result.append(total)
+    return result
+
 def compute_average(*args):
     sum = 0
     for argument in args: sum += argument
