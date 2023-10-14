ce7db89c1afc57387313515b2119cecdf7a2b9b9
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-13 20:35:42 -0600

    Make ordered grids provide information

--- Grid.py
+++ Grid.py
@@ -1,4 +1,5 @@
 from .fire_chicken.mouse_position import MousePosition
+from typing import List
 
 class Rectangle:
     def __init__(self, top, bottom, left, right):
@@ -14,7 +15,11 @@ class Grid:
 class VerticallyOrderedGrid:
     def compute_absolute_position_above_grid_position_by_vertical_amount(self, grid_coordinates: str, amount: int) -> MousePosition: pass
     def compute_absolute_position_below_grid_position_by_vertical_amount(self, grid_coordinates: str, amount: int) -> MousePosition: pass
+    def get_vertical_coordinates(self) -> List: pass
+    def compute_absolute_vertical_from(self, coordinate) -> int: pass
 
 class HorizontallyOrderedGrid:
     def compute_absolute_position_to_the_right_of_grid_position_by_horizontal_amount(self, grid_coordinates: str, amount: int) -> MousePosition: pass
     def compute_absolute_position_to_the_left_of_grid_position_by_horizontal_amount(self, grid_coordinates: str, amount: int) -> MousePosition: pass
+    def get_horizontal_coordinates(self) -> List: pass
+    def compute_absolute_horizontal_from(self, coordinate) -> int: pass
