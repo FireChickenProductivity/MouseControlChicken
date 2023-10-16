0897a66fa9992be925bd470938321c1811550be3
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-10-15 21:31:31 -0600

    Handle line divisors with class

--- RectangleUtilities.py
+++ RectangleUtilities.py
@@ -1,5 +1,6 @@
 from .Grid import Rectangle
 from .fire_chicken.mouse_position import MousePosition
+import math
 
 def compute_center_position(rectangle: Rectangle) -> MousePosition:
     horizontal = compute_average(rectangle.left, rectangle.right)
@@ -7,19 +8,25 @@ def compute_center_position(rectangle: Rectangle) -> MousePosition:
     center = MousePosition(int(horizontal), int(vertical))
     return center
 
-def compute_sub_rectangle(rectangle: Rectangle, number_of_horizontal_divisors: int, number_of_vertical_divisors: int, horizontal: int, vertical: int):
-    pass
+class LineDivider:
+    def __init__(self, start: int, ending: int, number_of_divisors: int):
+        self.start = start
+        self.ending = ending
+        self.number_of_divisors = number_of_divisors
+        self._compute_division_size()
+    
+    def _compute_division_size(self):
+        self.division_size = math.ceil(self.ending - self.start)/(self.number_of_divisors + 1)
+        if self.start + int(self.division_size*self.number_of_divisors) > self.ending: 
+            self.number_of_divisors = math.floor((self.ending - self.start)/(self.division_size))
 
-def compute_line_divisors(start_amount: int, ending_amount: int, number_of_divisors: int):
-    if start_amount == ending_amount: return [start_amount]
-    division_size = int((ending_amount - start_amount)/(number_of_divisors + 1))
-    current_position = start_amount
-    result = []
-    for divisor in range(number_of_divisors):
-        if current_position > ending_amount: break
-        total += division_size
-        result.append(total)
-    return result
+    def compute_divisor_position(self, number: int) -> int:
+        position = self.start + self.division_size*number
+        return position
+
+    def get_number_of_divisors(self) -> int:
+        return self.number_of_divisors
+
 
 def compute_average(*args):
     sum = 0
