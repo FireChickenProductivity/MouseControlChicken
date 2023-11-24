18608fbdb9fc130ebcfda3fabe7b3a706103185e
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-11-24 07:49:01 -0700

    Add return coordinates methods

--- source/RectangularGrid.py
+++ source/RectangularGrid.py
@@ -8,6 +8,8 @@ class ListBasedGrid(RectangularGrid):
         Separator is used for parsing horizontal versus vertical. Empty string separator means single character coordinates
     '''
     def __init__(self, horizontal_list: List, vertical_list: List, separator: str = ""):
+        self.horizontal_list = horizontal_list
+        self.vertical_list = vertical_list
         self.horizontal_coordinates = create_ordering_dictionary(horizontal_list)
         self.vertical_coordinates = create_ordering_dictionary(vertical_list)
         self.rectangle = None
@@ -25,6 +27,12 @@ class ListBasedGrid(RectangularGrid):
         vertical = self.compute_absolute_vertical_from(grid_coordinates)
         position = MousePosition(horizontal, vertical)
         return position
+    
+    def get_horizontal_coordinates(self) -> List:
+        return self.horizontal_list[:]
+
+    def get_vertical_coordinates(self) -> List:
+        return self.vertical_list[:]
 
     def compute_absolute_horizontal_from(self, coordinates: str) -> int: 
         horizontal_coordinate = self._compute_horizontal_coordinate(coordinates)
