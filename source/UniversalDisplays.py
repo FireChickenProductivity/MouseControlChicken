3cd329bd27d9bd470d8669d2fb5fb9234cab693f
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-11-26 16:18:12 -0700

    Remove unused class

--- source/UniversalDisplays.py
+++ source/UniversalDisplays.py
@@ -3,22 +3,6 @@ from .Grid import Grid, Rectangle
 from .fire_chicken.mouse_position import MousePosition
 from .Canvas import Canvas, Text, Line, CanvasElementOptions
 
-class LabeledPosition:
-    def __init__(self, position: MousePosition, label: str):
-        self.label = label
-        self.horizontal = position.get_horizontal()
-        self.vertical = position.get_vertical()
-    
-    def get_label(self) -> str:
-        return self.label
-
-    def get_horizontal(self) -> int:
-        return self.horizontal
-
-    def get_vertical(self) -> int:
-        return self.vertical
-
-
 class UniversalPositionDisplay(PositionDisplay):
     def __init__(self):
         self.grid: Grid = None
