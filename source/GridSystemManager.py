8eb63d1eb9d3395704f72dee40b777e7d976d331
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-15 21:02:31 -0700

    Add rectangle manager commands

--- source/GridSystemManager.py
+++ source/GridSystemManager.py
@@ -1,12 +1,14 @@
 from .Grid import Grid
 from .Display import Display
 from .RectangleManager import RectangleManager, ScreenRectangleManager, CurrentWindowRectangleManager
+from .SettingsMediator import settings_mediator
+from talon import Module, actions
 
 class GridSystemManager:
     def __init__(self):
         self.grid: Grid = None
         self.display: Display = None
-        self.rectangle_manager: RectangleManager = ScreenRectangleManager
+        self.rectangle_manager: RectangleManager = ScreenRectangleManager()
     
     def set_grid(self, grid: Grid):
         self.grid = grid
@@ -26,3 +28,16 @@ class GridSystemManager:
             self.display.hide()
             self.display.set_grid(self.grid)
             self.display.show()
+
+manager = GridSystemManager()
+
+module = Module()
+@module.action_class
+class Actions:
+    def mouse_control_chicken_make_grid_around_screen():
+        '''Makes the mouse control chicken grid form around a screen'''
+        manager.set_rectangle_manager(ScreenRectangleManager())
+    
+    def mouse_control_chicken_make_grid_around_window():
+        '''Makes the mouse control chicken grid form around the current window'''
+        manager.set_rectangle_manager(CurrentWindowRectangleManager())
