1838e343bcca0a2c9f61141a1868adf93ae9022f
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-18 19:40:40 -0700

    Add simple coordinate systems

--- source/InputCoordinateSystem.py
+++ source/InputCoordinateSystem.py
@@ -1,18 +1,41 @@
-from typing import Generator, Tuple
+from typing import Generator, Tuple, List
 
 class InputCoordinateSystem:
-    def get_primary_coordinates() -> Generator:
+    def get_primary_coordinates(self) -> Generator:
         pass
     
-    def do_coordinates_belong_to_system(coordinates: str) -> bool:
+    def do_coordinates_belong_to_system(self, coordinates: str) -> bool:
         pass
     
-    def do_coordinates_start_belong_to_system(coordinates: str) -> bool:
+    def do_coordinates_start_belong_to_system(self, coordinates: str) -> bool:
         pass
     
-    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system() -> Tuple[str]:
+    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(self, coordinates: str) -> Tuple[str]:
         pass
     
-    def get_name() -> str:
-        pass
+    def compute_coordinate_list(self, coordinates: str) -> List[str]:
+        coordinate_list = coordinates.split(self.separator)
+        return coordinate_list
+
+class ListCoordinateSystem(InputCoordinateSystem):
+    def __init__(self, coordinate_list: List[str], separator: str = " "):
+        self.coordinates = set(coordinate_list)
+        self.separator = separator
+    
+    def get_primary_coordinates(self) -> Generator:
+        for coordinate in self.coordinates: yield coordinate
+
+    def do_coordinates_belong_to_system(self, coordinates: str) -> bool:
+        coordinate_list = self.compute_coordinate_list(coordinates)
+        return len(coordinate_list) == 1 and coordinate_list[0] in self.coordinates
+
+    def do_coordinates_start_belong_to_system(self, coordinates: str) -> bool:
+        coordinate_list = self.compute_coordinate_list(coordinates)
+        return coordinate_list[0] in self.coordinates
+
+    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(self, coordinates: str) -> Tuple[str]:
+        coordinate_list = self.compute_coordinate_list(coordinates)
+        return (coordinate_list[0], self.separator.join(coordinate_list[1:]))
+
+
 
