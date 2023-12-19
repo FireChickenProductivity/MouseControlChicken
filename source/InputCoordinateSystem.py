48ffc5fc32cab10a1dcee8de7f9117616431dbcd
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-18 17:28:17 -0700

    Add coordinates system interface

---
+++ source/InputCoordinateSystem.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class InputCoordinateSystem:
+    def get_primary_coordinates() -> List[str]:
+        pass
+    
+    def do_coordinates_belong_to_system(coordinates: str) -> bool:
+        pass
+    
+    def do_coordinates_start_belong_to_system(coordinates: str) -> bool:
+        pass
+    
+    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system() -> List[str]:
+        pass
+    
+    def get_name() -> str:
+        pass
+
