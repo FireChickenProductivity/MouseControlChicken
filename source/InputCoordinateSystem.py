c6f2dace7d7fac565074baffa4e2e38879683986
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-18 17:29:04 -0700

    Improve typing

--- source/InputCoordinateSystem.py
+++ source/InputCoordinateSystem.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import Generator, Tuple
 
 class InputCoordinateSystem:
-    def get_primary_coordinates() -> List[str]:
+    def get_primary_coordinates() -> Generator:
         pass
     
     def do_coordinates_belong_to_system(coordinates: str) -> bool:
@@ -10,7 +10,7 @@ class InputCoordinateSystem:
     def do_coordinates_start_belong_to_system(coordinates: str) -> bool:
         pass
     
-    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system() -> List[str]:
+    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system() -> Tuple[str]:
         pass
     
     def get_name() -> str:
