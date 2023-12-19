881da2d221bfa676f0a47ca423c1b86488b647e0
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-18 20:06:26 -0700

    Add sequential combination

--- source/InputCoordinateSystem.py
+++ source/InputCoordinateSystem.py
@@ -17,6 +17,35 @@ class InputCoordinateSystem:
         coordinate_list = coordinates.split(self.separator)
         return coordinate_list
 
+class SequentialCombinationCoordinateSystem:
+    def __init__(self, systems: List[InputCoordinateSystem], separator: str = " "):
+        self.systems = systems
+        self.separator = separator
+    
+    def do_coordinates_belong_to_system(self, coordinates: str) -> bool:
+        remaining_coordinates = coordinates
+        for index, system in enumerate(self.systems):
+            if not system.do_coordinates_start_belong_to_system(remaining_coordinates): return False
+            if system.do_coordinates_belong_to_system(remaining_coordinates): return index == len(self.systems) - 1
+            remaining_coordinates = system.split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(coordinates)[1]
+    
+    def do_coordinates_start_belong_to_system(self, coordinates: str) -> bool:
+        remaining_coordinates = coordinates
+        for index, system in enumerate(self.systems):
+            if not system.do_coordinates_start_belong_to_system(remaining_coordinates): return False
+            remaining_coordinates = system.split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(coordinates)[1]
+        return True
+    
+    def split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(self, coordinates: str) -> Tuple[str]:
+        remaining_coordinates = coordinates
+        head = ""
+        for index, system in enumerate(self.systems):
+            part_belonging_to_system, remaining_coordinates = system.split_coordinates_with_head_belonging_to_system_and_tail_belonging_to_another_system(coordinates)[1]
+            if head: head += self.separator
+            head += part_belonging_to_system
+        return part_belonging_to_system, remaining_coordinates
+
+
 class SingleCoordinateCoordinateSystem(InputCoordinateSystem):
     def do_coordinates_belong_to_system(self, coordinates: str) -> bool:
         coordinate_list = self.compute_coordinate_list(coordinates)
