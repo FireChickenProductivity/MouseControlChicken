c304513b9ac589525f6ea12f198ded0327f2563d
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-27 10:00:35 -0700

    Start on dictation input display

---
+++ source/DictationInputDisplay.py
@@ -0,0 +1,60 @@
+from talon import Module, actions, imgui
+from typing import Callable
+
+title = None
+dictation_input = None
+acceptance_callback: Callable[[str], None] = None
+cancellation_callback: Callable[[], None] = None
+
+@imgui.open(y = 0)
+def gui(gui: imgui.GUI):
+    if title: gui.text(title)
+    gui.line()
+    if dictation_input: gui.text(dictation_input)
+
+module = Module()
+@module.action_class
+class Actions:
+    def mouse_control_chicken_show_dictation_input_display_with_title_acceptance_callback_and_cancellation_callback(new_title: str, new_acceptance_callback: Callable[[str], None], new_cancellation_callback: Callable[[], None], tag: str = ""):
+        '''Shows dictation input for mouse control chicken'''
+        actions.user.mouse_control_chicken_hide_dictation_input_display()
+        global title
+        title = new_title
+        global acceptance_callback
+        acceptance_callback = new_acceptance_callback
+        global cancellation_callback
+        cancellation_callback = new_cancellation_callback
+        actions.user.mouse_control_chicken_enable_dictation_input_display_tag()
+
+    def mouse_control_chicken_hide_dictation_input_display():
+        '''Hides the dictation input display for mouse control chicken'''
+        actions.user.mouse_control_chicken_disable_dictation_input_display_tag()
+
+    def mouse_control_chicken_accept_dictation_input():
+        '''Accepts the current dictation input for mouse control chicken'''
+        global acceptance_callback
+        acceptance_callback(dictation_input)
+        actions.user.mouse_control_chicken_hide_dictation_input_display()
+        erase_dictation_input_data()
+
+    def mouse_control_chicken_cancel_dictation_input():
+        '''Cancels taking dictation input for mouse control chicken'''
+        global cancellation_callback
+        cancellation_callback()
+        actions.user.mouse_control_chicken_hide_dictation_input_display()
+        erase_dictation_input_data()
+
+    def mouse_control_chicken_set_dictation_input_display_text(text: str):
+        '''Sets the text of the mouse control chicken dictation input display'''
+        global dictation_input
+        dictation_input = text
+    
+def erase_dictation_input_data():
+    global dictation_input
+    dictation_input = None
+    global title
+    title = None
+    global acceptance_callback
+    acceptance_callback = None
+    global cancellation_callback
+    cancellation_callback = None
