c820e37aea04ab07995fe2540d74ae90580741a5
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-28 13:47:38 -0700

    Reorganize code

--- source/GridFactoryArgumentTypes.py
+++ source/GridFactoryArgumentTypes.py
@@ -54,29 +54,3 @@ module = Module()
 def mouse_control_chicken_grid_factory_argument(m) -> str:
     return ""
 
-def create_context_matches_single_tag_string(tag: str) -> str:
-    return "tag:" + tag
-
-two_to_nine_context = Context()
-two_to_nine_context.matches = create_context_matches_single_tag_string(GRID_CREATION_ARGUMENT_TWO_TO_NINE_TAG)
-@two_to_nine_context.capture(CAPTURE_NAME, rule = "two|three|four|five|six|seven|eight|nine")
-def mouse_control_chicken_grid_factory_argument(m) -> str:
-    number_text = m[0]
-    if number_text == "two":
-        return "2"
-    elif number_text == "three":
-        return "3"
-    elif number_text == "four":
-        return "4"
-    elif number_text == "five":
-        return "5"
-    elif number_text == "six":
-        return "6"
-    elif number_text == "seven":
-        return "7"
-    elif number_text == "eight":
-        return "8"
-    elif number_text == "nine":
-        return "9"
-    else:
-        return ""
