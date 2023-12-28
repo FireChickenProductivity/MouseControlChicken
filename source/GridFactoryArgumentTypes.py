fa0b3c5eead3d202681b16450b851e26a6306f4d
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-28 10:33:01 -0700

    Add argument type capture

--- source/GridFactoryArgumentTypes.py
+++ source/GridFactoryArgumentTypes.py
@@ -1,6 +1,6 @@
 from .TagManagement import GRID_CREATION_ARGUMENT_TWO_TO_NINE_TAG, GRID_CREATION_ARGUMENT_GRID_OPTION_TAG
 from .GridOptions import GridOptions
-from talon import actions
+from talon import actions, Context, Module
 from typing import List
 
 
@@ -47,3 +47,36 @@ class GridOptionArgumentType(FactoryArgumentType):
     def get_options(self) -> List[str]:
         options: GridOptions = actions.user.mouse_control_chicken_get_grid_options()
         return options.get_option_names()
+
+CAPTURE_NAME = "mouse_control_chicken_grid_factory_argument"
+module = Module()
+@module.capture(rule = "placeholder")
+def mouse_control_chicken_grid_factory_argument(m) -> str:
+    return ""
+
+def create_context_matches_single_tag_string(tag: str) -> str:
+    return "tag:" + tag
+
+two_to_nine_context = Context()
+two_to_nine_context.matches = create_context_matches_single_tag_string(GRID_CREATION_ARGUMENT_TWO_TO_NINE_TAG)
+@two_to_nine_context.capture(CAPTURE_NAME, rule = "two|three|four|five|six|seven|eight|nine")
+def mouse_control_chicken_grid_factory_argument(m) -> str:
+    number_text = m[0]
+    if number_text == "two":
+        return "2"
+    elif number_text == "three":
+        return "3"
+    elif number_text == "four":
+        return "4"
+    elif number_text == "five":
+        return "5"
+    elif number_text == "six":
+        return "6"
+    elif number_text == "seven":
+        return "7"
+    elif number_text == "eight":
+        return "8"
+    elif number_text == "nine":
+        return "9"
+    else:
+        return ""
