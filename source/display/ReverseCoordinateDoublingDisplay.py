5436cc9e28f2313748392ae3b99fe625bbaa17a3
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-05-24 20:58:39 -0600

    Add initial boundary handling

--- source/display/ReverseCoordinateDoublingDisplay.py
+++ source/display/ReverseCoordinateDoublingDisplay.py
@@ -1,4 +1,4 @@
-from .Display import Display
+from .Display import Display, BoundariesTouching, compute_boundaries_touching
 from copy import deepcopy
 from ..grid.ReverseCoordinateDoublingGrid import ReverseCoordinateDoublingGrid
 from .Canvas import Canvas
@@ -10,12 +10,26 @@ class ReverseCoordinateDoublingDisplay(Display):
         self.secondary_display = deepcopy(display)
     
     def draw_on(self, canvas: Canvas):
-        self.primary_display.set_grid(self.grid.get_primary_grid())
+        primary_grid = self.grid.get_primary_grid()
+        self.primary_display.set_grid(primary_grid)
         self.primary_display.set_rectangle(self.grid.get_primary_rectangle())
-        self.secondary_display.set_grid(self.grid.get_secondary_grid())
+        secondary_grid = self.grid.get_secondary_grid()
+        self.secondary_display.set_grid(secondary_grid)
         self.secondary_display.set_rectangle(self.grid.get_secondary_rectangle())
-        self.primary_display.draw_on(canvas)
-        self.secondary_display.draw_on(canvas)
+        if self.primary_display.is_boundary_acknowledging():
+            boundaries_touching = compute_boundaries_touching(
+                self.grid.get_primary_rectangle(),
+                self.grid.get_rectangle(),
+            )
+            self.primary_display.draw_on_canvas_given_boundaries_touching(canvas, boundaries_touching)
+            boundaries_touching = compute_boundaries_touching(
+                self.grid.get_secondary_rectangle(),
+                self.grid.get_rectangle(),
+            )
+            self.secondary_display.draw_on_canvas_given_boundaries_touching(canvas, boundaries_touching)
+        else:
+            self.primary_display.draw_on(canvas)
+            self.secondary_display.draw_on(canvas)
     
     @staticmethod
     def supports_grid(grid):
