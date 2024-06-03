6c4fb0ae7d3d9ab72b40516fa785802e20b10bc1
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2024-06-03 13:43:16 -0600

    Simplify handling display halfs

--- source/display/ReverseCoordinateDoublingDisplay.py
+++ source/display/ReverseCoordinateDoublingDisplay.py
@@ -9,45 +9,66 @@ def is_node_recursive_doubling_grid_node(node: Node) -> bool:
     grid = node.get_value()
     return isinstance(grid, ReverseCoordinateDoublingGrid)
 
+class ReverseCoordinateDoublingHalfDisplay():
+    def __init__(self, display: Display):
+        self.rectangle = None
+        self.display = display
+        self.main_rectangle = None
+    
+    def set_grid(self, grid: Grid):
+        self.rectangle = grid.get_rectangle()
+        self.display.set_rectangle(self.rectangle)
+        self.display.set_grid(grid)
+    
+    def set_main_rectangle(self, main_rectangle):
+        self.main_rectangle = main_rectangle
+
+    def draw_on(self, canvas: Canvas):
+        if self.display.is_boundary_acknowledging():
+            boundaries_touching = compute_boundaries_touching(
+                self.rectangle,
+                self.main_rectangle,
+            )
+            self.display.draw_on_canvas_given_boundaries_touching(canvas, boundaries_touching)
+        else:
+            self.display.draw_on(canvas)
+        
+    def get_name(self) -> str:
+        return self.display.get_name()
+
 class ReverseCoordinateDoublingDisplay(Display):
     def __init__(self, display: Display):
         super().__init__()
-        self.primary_display = display
-        self.secondary_display = deepcopy(display)
+        self.primary_display = ReverseCoordinateDoublingHalfDisplay(display)
+        self.secondary_display = ReverseCoordinateDoublingHalfDisplay(deepcopy(display))
     
+    def _update_grid_value(self, grid: Grid):
+        self.primary_display.set_grid(grid.get_primary_grid())
+        self.secondary_display.set_grid(grid.get_secondary_grid())
+
     def set_grid(self, grid: Grid):
         tree = compute_grid_tree(grid)
         if is_node_recursive_doubling_grid_node(tree):
-            super().set_grid(tree.get_value())
+            self._update_grid_value(tree.get_value())
         else:
             matching_node = find_first_grid_tree_node_matching_function(tree, is_node_recursive_doubling_grid_node)
             if matching_node:
                 matching_grid = matching_node.get_value()
-                super().set_grid(matching_grid)
+                self._update_grid_value(matching_grid)
             else:
                 raise GridNotFoundException('Grid is not a recursive doubling grid: ' + str(grid) + '.')
 
+    def set_rectangle(self, rectangle):
+        for display in self.get_displays():
+            display.set_main_rectangle(rectangle)
+
     def draw_on(self, canvas: Canvas):
-        primary_grid = self.grid.get_primary_grid()
-        self.primary_display.set_grid(primary_grid)
-        self.primary_display.set_rectangle(self.grid.get_primary_rectangle())
-        secondary_grid = self.grid.get_secondary_grid()
-        self.secondary_display.set_grid(secondary_grid)
-        self.secondary_display.set_rectangle(self.grid.get_secondary_rectangle())
-        if self.primary_display.is_boundary_acknowledging():
-            boundaries_touching = compute_boundaries_touching(
-                self.grid.get_primary_rectangle(),
-                self.grid.get_rectangle(),
-            )
-            self.primary_display.draw_on_canvas_given_boundaries_touching(canvas, boundaries_touching)
-            boundaries_touching = compute_boundaries_touching(
-                self.grid.get_secondary_rectangle(),
-                self.grid.get_rectangle(),
-            )
-            self.secondary_display.draw_on_canvas_given_boundaries_touching(canvas, boundaries_touching)
-        else:
-            self.primary_display.draw_on(canvas)
-            self.secondary_display.draw_on(canvas)
+        displays = self.get_displays()
+        for display in displays:
+            display.draw_on(canvas)
+        
+    def get_displays(self):
+        return [self.primary_display, self.secondary_display]
     
     @staticmethod
     def supports_grid(grid):
