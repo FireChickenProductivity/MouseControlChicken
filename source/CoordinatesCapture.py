04917db6de09998e752cef44c101f1720b10b7c7
Fire Chicken <firechickenproductivity@gmail.com>
2024-08-12 17:36:15 -0600

    Start on overrides

--- source/CoordinatesCapture.py
+++ source/CoordinatesCapture.py
@@ -1,4 +1,4 @@
-from talon import Module
+from talon import Module, Context
 
 module = Module()
 module.list('mouse_control_chicken_uppercase_letter', desc="Upper case letters for use with the mouse control chicken grids")
@@ -22,8 +22,12 @@ def mouse_control_chicken_coordinates(m) -> str:
 module.list('mouse_control_chicken_empty_list', desc="An empty list")
 
 @module.capture(rule = "<number_small>+")
-def mouse_control_chicken_number_sequence(m) -> int:
-    return int(" ".join([str(x) for x in m]))
+def mouse_control_chicken_number_sequence(m) -> str:
+    return " ".join([str(x) for x in m])
+
+@module.capture(rule = "<number_small>")
+def mouse_control_chicken_single_number(m) -> str:
+    return str(m[0])
 
 @module.capture(rule = "<user.letter> <user.letter>")
 def mouse_control_chicken_lowercase_letter_pair(m) -> str:
@@ -40,3 +44,24 @@ def mouse_control_chicken_letter_pair(m) -> str:
 @module.capture(rule = "<user.mouse_control_chicken_letter_pair>")
 def mouse_control_chicken_main_coordinates(m) -> str:
     return " ".join(m)
+
+@module.capture(rule = "<user.letter> <user.letter>")
+def mouse_control_chickens_secondary_coordinates(m) -> str:
+    return " ".join(m)
+
+@module.capture(rule = "<user.mouse_control_chicken_letter_pair>")
+def mouse_control_chicken_tertiary_coordinates(m) -> str:
+    return " ".join(m)
+
+class CoordinateContext:
+    def __init__(self, input_coordinate_capture_name: str, coordinate_level: int = 1):
+        self.context = Context()
+        self.context.matches = f"""
+        tag: user.{input_coordinate_capture_name}_{coordinate_level}
+"""
+        
+override_contexts = []
+def build_override_contexts():
+    input_coordinate_capture_names = ["mouse_control_chicken_number_sequence", "mouse_control_chicken_lowercase_letter_pair", "mouse_control_chicken_letter_pair", "mouse_control_chicken_single_number"]
+    captures_to_override_by_level = {1: ["mouse_control_chicken_main_coordinates"], 2: ["mouse_control_chickens_secondary_coordinates"], 3: ["mouse_control_chicken_tertiary_coordinates"]}
+
