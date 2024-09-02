6f782abcf567e23efd7d0af9d262aedb064897c1
fire chicken <firechickenproductivity@gmail.com>
2024-09-02 16:21:52 -0600

    Fix creation of default file

--- source/SettingsFileManagement.py
+++ source/SettingsFileManagement.py
@@ -52,29 +52,41 @@ def create_settings_file_entries() -> List[SettingsFileEntry]:
     return [
         SettingsFileEntry(
             "user.mouse_control_chicken_default_grid_option",
-            "double alphabet numbers",
+            '"double alphabet numbers"',
             ["The name of the default grid option. This must be one of the grid options that shows up when you open the grid options list."]
         ),
         SettingsFileEntry(
             "user.mouse_control_chicken_font",
-            "lucida sans typewriter",
+            '"lucida sans typewriter"',
             ["The default font option makes it easier to differentiate between I and l.",
             "If you do not like it, you could try 'arial rounded mt', which is the default font used by the flex grid"]
         ),
         SettingsFileEntry("user.mouse_control_chicken_default_text_size", "10"),
-        SettingsFileEntry("user.mouse_control_chicken_default_text_color", "66ff00"),
+        SettingsFileEntry("user.mouse_control_chicken_default_text_color", '"66ff00"'),
         SettingsFileEntry("user.mouse_control_chicken_default_line_width", "1"),
-        SettingsFileEntry("user.mouse_control_chicken_default_line_color", "FF0000"),
+        SettingsFileEntry("user.mouse_control_chicken_default_line_color", '"FF0000"'),
         SettingsFileEntry("user.mouse_control_chicken_default_line_transparency", "0.5"),
         SettingsFileEntry("user.mouse_control_chicken_default_background_transparency", "0.50"),
-        SettingsFileEntry("user.mouse_control_chicken_default_background_color", "000000"),
+        SettingsFileEntry("user.mouse_control_chicken_default_background_color", '"000000"'),
         SettingsFileEntry("user.mouse_control_chicken_default_main_transparency", "0"),
-        SettingsFileEntry("user.mouse_control_chicken_default_current_screen_number", "0"),
-        SettingsFileEntry("user.mouse_control_chicken_default_frame_grid_offset", "10"),
+        SettingsFileEntry(
+            "user.mouse_control_chicken_default_current_screen_number",
+            "0",
+            ["This determines the default screen that the grid will be shown on."]
+        ),
+        SettingsFileEntry(
+            "user.mouse_control_chicken_default_frame_grid_offset",
+            "10",
+            ["This determines how far from the frame the text is in a frame grid display."],
+        ),
         SettingsFileEntry("user.mouse_control_chicken_default_frame_grid_should_show_crisscross", "false"),
         SettingsFileEntry("user.mouse_control_chicken_default_horizontal_frame_proximity_distance", "350"),
         SettingsFileEntry("user.mouse_control_chicken_default_vertical_frame_proximity_distance", "400"),
-        SettingsFileEntry("user.mouse_control_chicken_default_checker_frequency", "3"),
+        SettingsFileEntry(
+            "user.mouse_control_chicken_default_checker_frequency",
+            "3",
+            ["Every nth position is shown on a checker display where n is the frequency."],
+        ),
         SettingsFileEntry("user.mouse_control_chicken_default_zigzag_threshold", "0"),
         SettingsFileEntry("user.mouse_control_chicken_scrolling_amount", "600"),
         SettingsFileEntry("user.mouse_control_chicken_flickering_enabled", "false"),
