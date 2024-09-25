1139dc89ba0e8eaab12b9e4c71833d2304bfc8f2
fire chicken <firechickenproductivity@gmail.com>
2024-09-24 21:38:39 -0600

    Add scroll mode definition

--- source/ModeManagement.py
+++ source/ModeManagement.py
@@ -5,15 +5,35 @@ narrow_able_grid_mode_name = "mouse_control_chicken_narrow_able_grid_mode"
 narrow_able_grid_mode = 'user.' + narrow_able_grid_mode_name
 module.mode(narrow_able_grid_mode_name, desc = "A mode for working with a narrow able mouse control chicken grid")
 
+scroll_mode_name = "mouse_control_chicken_scroll_mode"
+scroll_mode = 'user.' + scroll_mode_name
+module.mode(scroll_mode_name, desc = "The mode activated during mouse control chicken repeated scrolling")
+
+def enable_mode(mode: str):
+    actions.mode.save()
+    actions.mode.disable("command")
+    actions.mode.disable("dictation")
+    actions.mode.enable(mode)
+
+def disable_mode(mode: str):
+    actions.mode.disable(mode)
+    actions.mode.restore()
+
 module = Module()
 @module.action_class
 class Actions:
     def mouse_control_chicken_enable_narrow_able_grid_mode():
         '''Enables the narrow able mouse control chicken grid mode'''
-        actions.mode.save()
-        actions.mode.disable("command")
-        actions.mode.disable("dictation")
-        actions.mode.enable(narrow_able_grid_mode)
+        enable_mode(narrow_able_grid_mode)
+    
+    def mouse_control_chicken_enable_scroll_mode():
+        '''Enables the mouse control chicken scroll mode'''
+        enable_mode(scroll_mode)
+    
+    def mouse_control_chicken_disable_scroll_mode():
+        '''Disables the mouse control chicken scroll mode'''
+        disable_mode(scroll_mode)
+    
     
     def mouse_control_chicken_disable_narrow_able_grid_mode():
         '''Disables the narrow able mouse control chicken grid mode'''
