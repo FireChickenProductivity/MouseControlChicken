245e5ac0f76f2b06b855e3212c9cfb25176039d7
fire chicken <firechickenproductivity@gmail.com>
2024-10-08 19:50:54 -0600

    Add instant scroll during continuous scrolling commands

--- source/Scrolling.py
+++ source/Scrolling.py
@@ -1,19 +1,24 @@
 from talon import Module, actions, cron
 from .SettingsMediator import settings_mediator
 
-SCROLLING_TIME_UNIT = "100ms"
+SCROLLING_TIME_UNIT = 100
 
 class ScrollingState:
     def __init__(self, speed: float, is_direction_down: bool = True, acceleration: int = 0):
         self.speed = speed
         self.is_direction_down = is_direction_down
         self.acceleration = acceleration
-    
-def scroll(scrolling_state: ScrollingState):
-    value = scrolling_state.speed*settings_mediator.get_continuous_scrolling_unit()
-    if not scrolling_state.is_direction_down:
-        value = -value
+
+def adjusts_scrolling_value_based_on_direction(value, is_direction_down):
+    return value if is_direction_down else -value
+
+def scroll_amount(value, is_direction_down):
+    value *= settings_mediator.get_continuous_scrolling_unit()
+    value = adjusts_scrolling_value_based_on_direction(value, is_direction_down)
     actions.mouse_scroll(value)
+
+def scroll(scrolling_state: ScrollingState):
+    scroll_amount(scrolling_state.speed, scrolling_state.is_direction_down)
     scrolling_state.speed += scrolling_state.acceleration
     
 scrolling_state: ScrollingState = None
@@ -28,7 +33,7 @@ class Actions:
         global scrolling_state
         scrolling_state = ScrollingState(speed, is_direction_down, acceleration)
         global scrolling_job
-        scrolling_job = cron.interval(SCROLLING_TIME_UNIT, lambda: scroll(scrolling_state))
+        scrolling_job = cron.interval(f"{SCROLLING_TIME_UNIT}ms", lambda: scroll(scrolling_state))
         actions.user.mouse_control_chicken_enable_scroll_mode()
     
     def mouse_control_chicken_reverse_scrolling():
@@ -54,3 +59,11 @@ class Actions:
         """Multiply the speed of mouse control chicken scrolling by the given factor."""
         if scrolling_state:
             scrolling_state.speed = scrolling_state.speed*factor
+        
+    def mouse_control_chicken_scroll_amount_during_continuous_scrolling(amount: int, is_direction_same: bool = True):
+        """Immediately scrolls the specified amount in the specified direction during continuous scrolling."""
+        direction = scrolling_state.is_direction_down
+        second_adjusted_amount = amount*(1000/SCROLLING_TIME_UNIT)
+        if not is_direction_same:
+            direction = not direction
+        scroll_amount(second_adjusted_amount, direction)
