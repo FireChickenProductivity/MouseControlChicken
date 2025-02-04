072bcda249f5fb940c59074255d747dad68cac4e
Fire Chicken <firechickenproductivity@gmail.com>
2025-02-04 12:03:28 -0700

    Simplify category computations

--- source/input_coordinates/CoordinatesCapture.py
+++ source/input_coordinates/CoordinatesCapture.py
@@ -221,25 +221,31 @@ def compute_tag_for_coordinate_system_category_and_depth(category: InputCoordina
         return None
     return f"user.{category_tag_start}_{depth}"
 
-def _append_tree_node_category_to_list(tree: Node, input_list: list):
+def _append_single_child_tree_node_category_to_list(tree: Node, input_list: list, compute_property):
     number_of_children = len(tree.get_children())
     if number_of_children < 2:
         node_grid = tree.get_value()
         coordinate_system = node_grid.get_coordinate_system()
-        category = coordinate_system.get_category()
-        input_list.append(category)
+        node_property = compute_property(coordinate_system)
+        input_list.append(node_property)
 
-def compute_categories(grid: Grid):
+def _append_tree_node_category_to_list(tree: Node, input_list: list):
+    _append_single_child_tree_node_category_to_list(tree, input_list, lambda coordinate_system: coordinate_system.get_category())
+
+def _compute_properties_from_coordinate_systems(grid: Grid, append_property_to_list):
     result = []
     tree_computation_options = TreeComputationOptions(keep_coordinate_system_modifying_wrappers=True)
     tree = compute_grid_tree(grid, tree_computation_options)
     while tree:
-        _append_tree_node_category_to_list(tree, result)
+        append_property_to_list(tree, result)
         if tree.has_children():
             tree = tree.get_children()[0]
         else:
             tree = None
     return result
+
+def compute_categories(grid: Grid):
+    return _compute_properties_from_coordinate_systems(grid, _append_tree_node_category_to_list)
 
 def compute_category_tags(grid: Grid):
     update_custom_coordinate_system(grid)
@@ -271,27 +277,14 @@ def compute_appropriate_level_tag_from_category_tags(category_tags):
     return None
 
 def _append_custom_coordinate_system_name_to_list(tree: Node, input_list: list):
-    number_of_children = len(tree.get_children())
-    if number_of_children < 2:
-        node_grid = tree.get_value()
-        coordinate_system = node_grid.get_coordinate_system()
+    def compute_name(coordinate_system: InputCoordinateSystem):
         if coordinate_system.is_custom():
-            name = coordinate_system.get_custom_coordinate_name()
-        else:
-            name = ""
-        input_list.append(name)
+            return coordinate_system.get_custom_coordinate_name()
+        return ""
+    _append_single_child_tree_node_category_to_list(tree, input_list, compute_name)
 
 def compute_custom_ordinate_system_names(grid: Grid):
-    result = []
-    tree_computation_options = TreeComputationOptions(keep_coordinate_system_modifying_wrappers=True)
-    tree = compute_grid_tree(grid, tree_computation_options)
-    while tree:
-        _append_custom_coordinate_system_name_to_list(tree, result)
-        if tree.has_children():
-            tree = tree.get_children()[0]
-        else:
-            tree = None
-    return result
+    return _compute_properties_from_coordinate_systems(grid, _append_custom_coordinate_system_name_to_list)
 
 def update_custom_coordinate_system(grid: Grid):
     names = compute_custom_ordinate_system_names(grid)
